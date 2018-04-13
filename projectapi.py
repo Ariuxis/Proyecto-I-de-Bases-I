8a0f1655155fca68287250da16ae17b9ef266bfe
Antonio Yu <antonio.yu1998@gmail.com>
2018-04-12 19:39:37 -0500

    WIP. Created the choose product function. Completed first requirement of the project.

--- projectapi.py
+++ projectapi.py
@@ -5,42 +5,89 @@ from cqlengine.management import sync_table
 from cqlengine import connection
 import csv
 
-class Client(Model):
-	idClient = columns.Integer(primary_key = True)
-	clientName = columns.Text()
-	clientAddress = columns.Text()
-	clientEmail = columns.Text()
-	clientUsername = columns.Text()
-	clientPassword = columns.Text()
+# All of the select queries must be made in lower case, since each of the rows, when created, are made in lower case.
+# Using keyspace: truncate tablename; to delete every single row from a table.
+# Use clientid 309 for testing purposes.
 
-Client.create(idClient = 11234, clientName = "John", 
-	clientAddress = "Kra 5", clientEmail = "hotmail", clientUsername = "foo", clientPassword = "bar")
+shoppingCart = []
+
+def chooseProduct(clientUser, errandid):
+	cluster = Cluster(['127.0.0.1'])
+	session = cluster.connect("shop")
+	index = 1
+	categoryNames = session.execute("select categoryname from product")
+	categoryList = []
+	productNames = session.execute("select productname, categoryname from product")
+	productList = []
+	print("\nCategorias")
+	for category in categoryNames:
+		if(category.categoryname not in categoryList):
+			print("{}. {}".format(index, category.categoryname))
+			categoryList.append(category.categoryname)
+			index = index + 1
+	option = input("\nEscoja una de las opciones previas: ")
+	choose = categoryList[option - 1]
+	print("\nProductos")
+	index = 1
+	for product in productNames:
+		if(product.categoryname == choose):
+			productList.append(product.productname)
+			print("{}. {}".format(index, product.productname))
+			index = index + 1
+	option = input("\nEscoja una de las opciones previas: ")
+	choose = productList[option - 1]
+	quantity = input("Indique la cantidad a comprar: ")
+	session.execute("""
+		insert into errand(errandid, clientid, errandstate, erranddate, errandquantity)
+		""")
+	del productList[:]
+	del categoryList[:]
+	return choose, quantity, errandid
+
+def options():
+	errandid = 1
+	print("""
+		1. Ver mi carrito de compras.
+		2. 
+		""")
+	option = input("Escoja una de las opciones previas: ")
+	if(option == 1):
+		print("""
+			1. Agregar producto.
+			2. Remover producto.
+			3. Ver los productos.
+			""")
+		option = input("Escoja una de las opciones previas: ")
+		if(option == 1):
+			productQuantity = chooseProduct(309, errandid)
+			for i in range(productQuantity[1]):
+				shoppingCart.append(productQuantity[0])
+
 
 def main():
-	cluster = Cluster(['127.0.0.1'])
-	session = cluster.connect("")
-	session.execute("""
-		create keyspace if not exists shop with replication = {
-		'class' : 'SimpleStrategy',
-		'replication_factor' : 1
-		} 
-		""")
-	session.set_keyspace("shop")
-	# session.execute("""
-	# 	create table if not exists client (
-	# 		idClient int primary key,
-	# 		clientName text,
-	# 		clientAddress text,
-	# 		clientEmail text,
-	# 		clientUsername text,
-	# 		clientPassword text
-	# 	)
-	# 	""")
-	sync_table(Client)
-	# session.execute("""
-	# 	insert into client(idClient, clientName, clientAddress, clientEmail, clientUsername, clientPassword)
-	# 	values (11234, 'John', 'Kra 5', 'hotmail', 'foo', 'bar')
-	# 	""")
-	# result = session.execute("select * from client")
-	# print(result)
-# main()
+	options()
+
+main()
+
+
+# def main():
+# 	cluster = Cluster(['127.0.0.1'])
+# 	session = cluster.connect("shop")
+# 	session.execute("""
+# 		insert into client(clientid, clientname, clientaddress, clientemail, clientusername, clientpassword)
+# 		values (11234, 'John', 'Kra 5', 'hotmail', 'foo', 'bar')
+# 		""")
+# 	session.execute("""
+# 		insert into client(clientID, clientName, clientAddress, clientEmail, clientUsername, clientPassword)
+# 		values (5739, 'Karl', 'Kra55', 'hotmail', 'foobar', 'barfoo')
+# 		""")
+# 	session.execute("""
+# 		delete from client where clientid = 5739
+# 		""")
+# 	# result = session.execute("select clientID, clientName from client")
+# 	# for i in result:
+# 	# 	print(i.clientid)
+
+
+# tupleChosen = chooseProduct()
+# print(tupleChosen[0])
